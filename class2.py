9a4dba594354a50028c6cd70abc0472ca5ffce23
Saurabhdz <saurabhzambare@gmail.com>
2025-12-30 16:47:22 +0100

    added test code to repository

---
+++ class2.py
@@ -0,0 +1,17 @@
+print("Hello")
+
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+c = int(input("Enter 3rd number : "))
+
+if(a>b):
+    if(a>c):
+        print(a," is greatest")
+else:
+    if(b>c):
+        print(b," is greater")
+    else:
+        print(c, " is greater")
+
+
+
